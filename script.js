4c19e838e200add1003bbcd2d4a7e1647eb332dd
carrilloandre <andre.carrillo@speeracademy.org>
2016-11-02 21:36:53 -0500

    v.0.0.0.2 Removed .fadeIn & fixed .fadeOut
    I made my "comandante.jpg" image fade out on the main page of my website.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-    $('h3').mouseenter(function(){
-        $('h3').fadeTo('fast', 1);
-});
-
-$('h3').mouseleave(function(){
-    $('h3').fadeTo('fast', 0.5);
+    $('.comandante').click(function(){
+        $('.comandante').fadeOut('slow');
     });
 });
